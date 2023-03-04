7aff95edb0b414f9f84140b2225345f9e6797413
Ruan <ruan@murgatroyd.me>
2023-03-04 14:52:12 +0000

    Added noDemon, replaced sqlite w/ LowDB, more.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-// SQLite3
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
 
 // Express
-const express = require('express')
 const app = express()
 const port = 3000
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+// Configure lowdb to write to JSONFile
+const file = join(__dirname, "/db.json");
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+
+await db.read()
+
+// If db.json doesn't exist, db.data will be null
+// Use the code below to set default data
+db.data ||= { posts: [] }             // For Node >= 15.x
+
+// Create and query items using native JS API
+db.data.posts.push('hello world')
+const firstPost = db.data.posts[0]
+
+// Alternatively, you can also use this syntax if you prefer
+const { posts } = db.data
+posts.push('hello world')
 
 app.use(express.static('public'))
 app.get('/', function (req, res) {
@@ -14,3 +37,6 @@ app.get('/', function (req, res) {
 
 console.log('Listening on port ' + port + '...');
 app.listen(port)
+
+// Finally write db.data content to file
+await db.write()
---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+// Get all the buttons
+var buttons = document.getElementsByTagName("button");
+
+// Loop through the buttons
+for (var i = 0; i < buttons.length; i++) {
+    // Add a click event listener to each button
+    buttons[i].addEventListener("click", function() {
+        // Get the ID of the button that was clicked
+        var buttonId = this.id;
+
+        // Get all the divs
+        var divs = document.getElementsByTagName("div");
+
+        // Loop through the divs
+        for (var i = 0; i < divs.length; i++) {
+            // Get the ID of the div
+            var divId = divs[i].id;
+
+            // If the div's ID is the same as the button's ID
+            if (divId == buttonId) {
+                // Show the div
+                divs[i].classList.remove("hidden");
+            } else {
+                // Hide the div
+                divs[i].classList.add("hidden");
+            }
+        }
+    });
+}
